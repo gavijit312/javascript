3e32bf39c970488b5c0f454fd1162445ae5c5362
Avijit Ghosh <gavijit312@gmail.com>
2026-03-21 18:17:00 +0000

    Add initial JavaScript files for datatype and account management

---
+++ 02_Datatype.js
@@ -0,0 +1,7 @@
+"use strict";
+// alert('GM') we use alert in browser , we are using node
+let name = "Avijit"
+let age = 23
+let relationship = false
+
+console.log(typeof age);
---
+++ basic.js
@@ -0,0 +1,11 @@
+const accountID = 3456
+let acccountName = "Avijit@gmail.com"
+var accountPassword = "1712"
+accountCity = "Kolkata"
+
+console.log(accountID);
+/*
+Avoid using var 
+*/
+
+console.table([accountID, acccountName, accountPassword, accountCity]);
