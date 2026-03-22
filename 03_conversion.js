b32762be07e912b19822c9e9b86c28ca6bb56d68
Avijit Ghosh <gavijit312@gmail.com>
2026-03-22 13:00:54 +0000

    Add conversion examples and logging to 03_conversion.js

---
+++ 03_conversion.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+console.log(typeof score);
+
+let newScore = Number(score)
+console.log(newScore);
+
+let loggedIn = 1
+let booleanIn = Boolean(loggedIn)
+console.log(typeof booleanIn);
+
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter);
