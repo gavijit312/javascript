37ce0c7ad5a208d1ea1ab36217114a42b7f259dd
Avijit Ghosh <gavijit312@gmail.com>
2026-03-23 18:04:10 +0000

    Add nickname handling and prototype logging to string.js

--- string.js
+++ string.js
@@ -2,3 +2,7 @@ const name = "Avijit";
 const age = 23;
 
 console.log(`This is my name ${name} and my age is ${age}`);
+
+const nickname = new String("Oldmonk");
+console.log(nickname[3]);
+console.log(nickname.__proto__);
